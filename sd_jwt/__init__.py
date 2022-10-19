6d0eacbd7a0118b7bec8bc6828a194f9ce6ccedb
Daniel Fett <fett@danielfett.de>
2022-10-19 09:50:32 +0200

    Update sd_jwt/__init__.py

--- sd_jwt/__init__.py
+++ sd_jwt/__init__.py
@@ -2,6 +2,6 @@ __version__ = "0.2.1"
 
 DEFAULT_SIGNING_ALG = "RS256"
 SD_DIGESTS_KEY = "sd_digests"
-SD_II_CLAIMS_KEY = "sd_ii_disclosure"
-SD_HS_CLAIMS_KEY = "sd_hs_disclosure"
+SD_II_CLAIMS_KEY = "sd_ii_disclosures"
+SD_HS_CLAIMS_KEY = "sd_hs_disclosures"
 DIGEST_ALG_KEY = "sd_digest_derivation_alg"
