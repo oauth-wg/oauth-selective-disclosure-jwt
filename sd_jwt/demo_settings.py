0e160af29c22e8817e1c4299108b73806159c5b1
Daniel Fett <danielf@yes.com>
2022-12-09 09:27:44 +0100

    Remove kid, alg and use from keys

--- sd_jwt/demo_settings.py
+++ sd_jwt/demo_settings.py
@@ -7,21 +7,15 @@ DEFAULT_KTY = "EC"
 ISSUER_KEY_DICT = {
     "kty": "EC",
     "d": "Ur2bNKuBPOrAaxsRnbSH6hIhmNTxSGXshDSUD1a1y7g",
-    "use": "sig",
     "crv": "P-256",
-    "kid": "1",
     "x": "b28d4MwZMjw8-00CG4xfnn9SLMVMM19SlqZpVb_uNtQ",
     "y": "Xv5zWwuoaTgdS6hV43yI6gBwTnjukmFQQnJ_kCxzqk8",
-    "alg": "ES256"
 }
 
 HOLDER_KEY_DICT = {
     "kty": "EC",
     "d": "5K5SCos8zf9zRemGGUl6yfok-_NiiryNZsvANWMhF-I",
-    "use": "sig",
     "crv": "P-256",
-    "kid": "2",
     "x": "TCAER19Zvu3OHF4j4W4vfSVoHIP1ILilDls7vCeGemc",
     "y": "ZxjiWWbZMQGHVWKVQ4hbSIirsVfuecCE6t4jT9F2HZQ",
-    "alg": "ES256"
 }
